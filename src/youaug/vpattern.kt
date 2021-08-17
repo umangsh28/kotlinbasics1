4139d46fae339601ed21477b81bf3d701a9236fb
umangsh28 <im.mrstark08@gmail.com>
2021-08-18 01:55:33 +0530

    vpattern

---
+++ src/youaug/vpattern.kt
@@ -0,0 +1,17 @@
+package youaug
+
+import java.util.*
+
+fun main(){
+    val sc=Scanner(System.`in`)
+    val l=sc.nextInt()
+    val r=sc.nextInt()
+    for(i in 1..l){
+        for(j in 1..r){
+            println("$i.$j")
+        }
+
+    }
+
+
+}
