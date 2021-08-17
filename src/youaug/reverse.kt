68e0a47360c6ee0c0ce94b865b75a8d80f495d00
umangsh28 <im.mrstark08@gmail.com>
2021-08-18 02:17:16 +0530

    reverse

---
+++ src/youaug/reverse.kt
@@ -0,0 +1,13 @@
+package youaug
+
+import java.util.*
+
+
+fun main(args: Array<String>) {
+    val sc = Scanner(System.`in`)
+    val n = sc.next()
+    for (i in n.length - 1 downTo 0) {
+        print(n[i])
+    }
+}
+
